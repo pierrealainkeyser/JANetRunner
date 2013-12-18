e59257a3764f21cf693c60ea78ecb101bbd6fcf5
Pierre-Alain Keyser <pierrealain.keyser@gmail.com>
2013-12-18 23:20:36 +0100

    gestion de la distribution de l'application sous forme de répertoire

--- ANR/src/main/java/org/keyser/anr/AnrMain.java
+++ ANR/src/main/java/org/keyser/anr/AnrMain.java
@@ -1,18 +1,22 @@
 package org.keyser.anr;
 
+import java.net.URL;
+import java.security.ProtectionDomain;
+
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class AnrMain {
 	public static void main(String[] args) throws Exception {
-
 		Server s = new Server(8082);
 
+		ProtectionDomain domain = AnrMain.class.getProtectionDomain();
+		URL location = domain.getCodeSource().getLocation();
+
 		WebAppContext context = new WebAppContext();
-		context.setBaseResource(Resource.newClassPathResource("/"));
+		context.setWar(location.toExternalForm());
 		context.setContextPath("/");
-		
+
 		s.setHandler(context);
 
 		s.start();
