6938e7f0484ba7c51574d81cc98ee82fdfcc9655
pakeyser <pakeyser@gmail.com>
2015-03-19 12:44:58 +0100

    début anchor layout

---
+++ ANR/src/main/resources/static/js/anchorlayout.js
@@ -0,0 +1,39 @@
+AnchorLayout = { Vertical : { TOP : 1,MIDDLE:2, BOTTOM : -1 }, Horizontal:{LEFT : -1, RIGHT : 1,MIDDLE:2,} }
+
+function anchorLayout(options) {
+	options = options || {}
+	var padding = options.padding || 0;
+	var vertical = options.vertical || AnchorLayout.Vertical.MIDDLE;
+	var horizontal = options.horizontal || AnchorLayout.Horizontal.MIDDLE;
+	var minSize = options.minSize || new Size();
+	
+	return function(boxcontainer, childs) {
+		var origin = new Point();
+		var bounds = new Rectangle({point:origin,size:minSize});
+		if (childs.length > 0) {
+			var local = childs[0].local;
+			var size = local.size;
+			
+			bounds = new Rectangle({point:origin,size:size}).merge(bounds);
+			
+			// prise en compte du padding
+			bounds.grow(padding);
+
+			//on centre le nouveau point
+			var point = new Point();
+			if (vertical === AnchorLayout.Vertical.MIDDLE)
+				point.y = (bounds.size.height - size.height) / 2;
+			else if (vertical === AnchorLayout.Vertical.BOTTOM)
+				point.y = bounds.size.height - size.height;
+			
+			if (horizontal === AnchorLayout.Horizontal.MIDDLE)
+				point.x = (bounds.size.width - size.width) / 2;
+			else if (horizontal === AnchorLayout.Horizontal.RIGHT)
+				point.x = bounds.size.width - size.width;
+			
+			
+			local.moveTo(point);
+		}
+		boxcontainer.local.resizeTo(bounds);
+	}
+}
