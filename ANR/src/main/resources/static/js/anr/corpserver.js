5e06a2ac10870681e524a480adc82529d5298864
Pierre-Alain Keyser <pierrealain.keyser@gmail.com>
2015-03-28 17:33:28 +0100

    suite correctif mineure

--- ANR/src/main/resources/static/js/anr/corpserver.js
+++ ANR/src/main/resources/static/js/anr/corpserver.js
@@ -9,6 +9,11 @@ function(mix, layout, ui, geom, AnchorLayout, FlowLayout, CardContainerBox) {
 	}
 	mix(Ices, layout.AbstractBoxContainer);
 
+	function Upgrades(layoutManager) {
+		layout.AbstractBoxContainer.call(this, layoutManager, {}, new FlowLayout({ padding : 0, direction : FlowLayout.Direction.RIGHT, spacing : -30 }));
+	}
+	mix(Upgrades, layout.AbstractBoxContainer);
+
 	function CorpServer(layoutManager, def) {
 		this.def = def;
 		layout.AbstractBoxContainer.call(this, layoutManager, {}, new FlowLayout({ direction : FlowLayout.Direction.TOP, align : FlowLayout.Align.MIDDLE,
@@ -30,8 +35,7 @@ function(mix, layout, ui, geom, AnchorLayout, FlowLayout, CardContainerBox) {
 
 		this.mainContainer = new CardContainerBox(layoutManager, type, innerLayout);
 
-		this.upgrades = new layout.AbstractBoxContainer(layoutManager, {}, new FlowLayout(
-				{ padding : 0, direction : FlowLayout.Direction.RIGHT, spacing : -30 }));
+		this.upgrades = new Upgrades(layoutManager);
 		this.ices = new Ices(layoutManager);
 
 		var upgradesMinSize = new layout.AbstractBoxContainer(layoutManager, {}, new AnchorLayout({ padding : 3, minSize : new geom.Size(80, 111) }));
@@ -44,7 +48,6 @@ function(mix, layout, ui, geom, AnchorLayout, FlowLayout, CardContainerBox) {
 
 	mix(CorpServer, layout.AbstractBoxContainer);
 	mix(CorpServer, function() {
-
 	});
 
 	return CorpServer;
