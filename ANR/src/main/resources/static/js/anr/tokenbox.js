14f5fcb5a09b9025ee096643e77d331955a82d98
pakeyser <pakeyser@gmail.com>
2015-04-02 15:30:18 +0200

    suppression d'un composant inutile

--- ANR/src/main/resources/static/js/anr/tokenbox.js
+++ ANR/src/main/resources/static/js/anr/tokenbox.js
@@ -1,52 +1,5 @@
 define([ "mix", "jquery", "ui/jqueryboxsize", "ui/animateappeareancecss" ], function(mix, $, JQueryBoxSize, AnimateAppeareanceCss) {
-	function TokenBox(layoutManager, container, type, value, text) {
-		var innerToken = $("<span class='token " + key + "'>" + value + "</span>");
-		if (text) {
-			var wrapper = $("<div class='token-wrapper'/>");
-			var text = $("<span class='token-text'>" + text + "</span>");
-			innerToken.appendTo(wrapper);
-			text.appendTo(wrapper);
-			innerToken = wrapper;
-		}
-		JQueryBoxSize.call(this, layoutManager, innerToken);
-		AnimateAppeareanceCss.call(this, "bounceIn", "bounceOut");
 
-		// on se rajoute dans le container et pas dans le truc du parent
-		this.element.appendTo(container);
-
-		this.valueElement = this.element.find(".token");
-
-		// le type de token
-		this.tokenType = type;
-
-		// la valeur graphique
-		this.tokenValue = value;
-
-		// fait apparaitre le token
-		this.animateCss(this.element, "fadeInRight");
-	}
-
-	mix(TokenBox, JQueryBoxSize);
-	mix(TokenBox, AnimateAppeareanceCss);
-	mix(TokenBox, function() {
-		/**
-		 * Mise à jour de la valeur du token, uniquement en cas de changement
-		 */
-		this.setValue = function(value) {
-			if (this.tokenValue !== value) {
-				this.tokenValue = value;
-				if (!value) {
-					// suppression de l'élement
-					this.animateRemove(this.element, this.remove.bind(this));
-				} else {
-					// changement de la valeur
-					this.animateSwap(this.element, function() {
-						this.valueElement.text(value);
-					}.bind(this));
-				}
-			}
-		}
-	});
 
 	return TokenBox;
 });
--- ANR/src/main/resources/static/js/anr/tokencontainerbox.js
+++ ANR/src/main/resources/static/js/anr/tokencontainerbox.js
@@ -1,5 +1,55 @@
-define([ "mix", "./tokenbox", "./tokenmodel", "layout/abstractboxcontainer" ],// 
-function(mix, TokenBox, TokenModel, AbstractBoxContainer) {
+define([ "mix", "jquery", , "./tokenmodel", "layout/abstractboxcontainer", "ui/jqueryboxsize", "ui/animateappeareancecss" ],// 
+function(mix, $, TokenModel, AbstractBoxContainer, JQueryBoxSize, AnimateAppeareanceCss) {
+
+	function TokenBox(layoutManager, container, type, value, text) {
+		var innerToken = $("<span class='token " + key + "'>" + value + "</span>");
+		if (text) {
+			var wrapper = $("<div class='token-wrapper'/>");
+			var text = $("<span class='token-text'>" + text + "</span>");
+			innerToken.appendTo(wrapper);
+			text.appendTo(wrapper);
+			innerToken = wrapper;
+		}
+		JQueryBoxSize.call(this, layoutManager, innerToken);
+		AnimateAppeareanceCss.call(this, "bounceIn", "bounceOut");
+
+		// on se rajoute dans le container et pas dans le truc du parent
+		this.element.appendTo(container);
+
+		this.valueElement = this.element.find(".token");
+
+		// le type de token
+		this.tokenType = type;
+
+		// la valeur graphique
+		this.tokenValue = value;
+
+		// fait apparaitre le token
+		this.animateCss(this.element, "fadeInRight");
+	}
+
+	mix(TokenBox, JQueryBoxSize);
+	mix(TokenBox, AnimateAppeareanceCss);
+	mix(TokenBox, function() {
+		/**
+		 * Mise à jour de la valeur du token, uniquement en cas de changement
+		 */
+		this.setValue = function(value) {
+			if (this.tokenValue !== value) {
+				this.tokenValue = value;
+				if (!value) {
+					// suppression de l'élement
+					this.animateRemove(this.element, this.remove.bind(this));
+				} else {
+					// changement de la valeur
+					this.animateSwap(this.element, function() {
+						this.valueElement.text(value);
+					}.bind(this));
+				}
+			}
+		}
+	});
+
 	function TokenContainerBox(layoutManager, layoutFunction, elementContainer, includeText, tokenModel) {
 		AbstractBoxContainer.call(this, layoutManager, {}, layoutFunction);
 		this.tokenModel = tokenModel;
@@ -48,8 +98,8 @@ function(mix, TokenBox, TokenModel, AbstractBoxContainer) {
 					this.createToken(type, value);
 
 			}.bind(this));
-			
-			//TODO suppression de tous les tokenbox qui n'ont pas le bon type
+
+			// TODO suppression de tous les tokenbox qui n'ont pas le bon type
 		}
 
 		/**
