62c9db226e81a41833b467424142a5f0fcd112a3
pakeyser <pakeyser@gmail.com>
2015-04-08 14:15:32 +0200

    tentative

--- ANR/src/main/resources/static/js/anr/zoomcontainerbox.js
+++ ANR/src/main/resources/static/js/anr/zoomcontainerbox.js
@@ -1,21 +1,21 @@
 define([ "mix", "jquery", "layout/abstractboxcontainer", "layout/abstractbox", "layout/impl/flowLayout", "layout/impl/anchorlayout", "ui/jqueryboxsize",
-		"ui/animateappeareancecss", "./headercontainerbox", "./tokencontainerbox" ], //
-function(mix, $, AbstractBoxContainer, AbstractBox, FlowLayout, AnchorLayout, JQueryBoxSize, AnimateAppeareanceCss, HeaderContainerBox, TokenContainerBox) {
+		"ui/animateappeareancecss", "./headercontainerbox", "./tokencontainerbox" ,"ui/jquerytrackingbox"], //
+function(mix, $, AbstractBoxContainer, AbstractBox, FlowLayout, AnchorLayout, JQueryBoxSize,
+		AnimateAppeareanceCss, HeaderContainerBox, TokenContainerBox, JQueryTrackingBox) {
 
 	function ZoomedDetail(layoutManager,element, zoomed){
 		AbstractBoxContainer.call(this,layoutManager, {}, new FlowLayout({}));
 		
 		var tokens = new TokenContainerBox(layoutManager, new FlowLayout({}), element, true, zoomed.tokenModel);
 		
-		var tokensHeader=new HeaderContainerBox(layoutManager, tokens, "Tokens");
+		this.tokensHeader=new HeaderContainerBox(layoutManager, tokens, "Tokens");
 		this.addChild(tokensHeader);
 		tokensHeader.header.appendTo(element);
 	}
-	
 	mix(ZoomedDetail, AbstractBoxContainer);
 	
-	function ZoomContainerBox(layoutManager, zoomed) {
-		AbstractBoxContainer.call(this, layoutManager, {}, new FlowLayout({}));
+	function ZoomContainerBox(layoutManager, zoomed, text) {
+		AbstractBoxContainer.call(this, layoutManager, {}, new FlowLayout({direction:FlowLayout.Direction.BOTTOM}));
 		AnimateAppeareanceCss.call(this, "fadeIn", "fadeOut");
 
 		this.zoomed = zoomed;
@@ -24,25 +24,50 @@ function(mix, $, AbstractBoxContainer, AbstractBox, FlowLayout, AnchorLayout, JQ
 		this.element = $("<div class='zoombox'/>");
 		
 		var zoomedDetail=new ZoomedDetail(layoutManager,this.element,zoomed);
+		this.header=new JQueryBoxSize(layoutManager,$("<div class='header'>"+text+"</header>"));
+		this.header.element.appendTo(this.element);
 		
-		this.addChild(this.primaryContainer);
-		this.addChild(zoomedDetail);
+		this.actions=new AbstractBoxContainer(layoutManager,{},new FlowLayout({}));
 		
-		// rajout dans le main
-		this.primaryContainer.addChild(zoomed);
+		var actionsBox=new JQueryTrackingBox(layoutManager,$("<div class='actions'/>"));
+		actionsBox.element.appendTo(this.element);
+		
+		var mainRow=new AbstractBoxContainer(layoutManager,{},new FlowLayout({}));
+		
+		mainRow.addChild(this.primaryContainer);
+		mainRow.addChild(zoomedDetail);
+	
+		this.addChild(this.header);
+		this.addChild(mainRow);
+		this.addChild(this.actions);
+		
+		// rajout dans le main TODO à revoir
+		primaryContainer.addChild(zoomed);
 
 		this.watchContainerFunction = this.watchContainer.bind(this);
 		zoomed.observe(this.watchContainerFunction, [ AbstractBox.CONTAINER ]);
+		
+		
+		// permet de ne pas merger les positions de la pointe parente pour les
+		// elements rataché à this.element
+		this.mergeRemoveZoomed=function(moveTo){
+				var topLeft = this.screen.topLeft()
+				moveTo.add({x:-topLeft.x,y:-topLeft.y});
+		}.bind(this);
+		this.header.additionnalMergePosition=this.mergeRemoveZoomed;
+		actionsBox.additionnalMergePosition=this.mergeRemoveZoomed;
+		zoomedDetail.tokensHeader.additionnalMergePosition=this.mergeRemoveZoomed;
 	}
 
 	mix(ZoomContainerBox, AbstractBoxContainer);
 	mix(ZoomContainerBox, AnimateAppeareanceCss);
 	mix(ZoomContainerBox, function() {
+		
 
 		this.watchContainer = function() {
 			// permet d'observer les cartes
 			if(zoomed.container!===this.primaryContainer){
-				
+				//TODO la carte est fermer
 			}
 		}
 
